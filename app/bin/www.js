af82581973e81d4e816eec14c80d6f21fbc86f32
chobkyu <qudqud97@naver.com>
2022-09-14 00:20:19 +0900

    일단 소켓 통신 해봄

--- app/bin/www.js
+++ app/bin/www.js
@@ -1,6 +1,29 @@
 const app =require("../app");
 
 const port =process.env.PORT || 5000;
-app.listen(port,()=>{
+const server = app.listen(port,()=>{
     console.log('서버 가동');
 });
+
+const SocketIO = require('socket.io');
+
+const io = SocketIO(server,{path:'/socket.io'});
+
+io.on('connection',(socket)=>{
+    socket.on('disconnect',()=>{
+        console.log('클라이언트 접속 해제',socket.id);
+        clearInterval(socket.interval);
+        
+    });
+
+    socket.on('error',(error)=>{
+        console.error(error);
+    });
+
+    //* 클라이언트로부터 메시지 수신
+    socket.on('reply', (data) => { // reply라는 이벤트로 송신오면 메세지가 data인수에 담김
+         console.log(data);
+    });
+    
+    socket.emit('news','hello monkey');
+})
