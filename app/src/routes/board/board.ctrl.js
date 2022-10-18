fe22cca3222888ac292c7b75b9e73c3a968342ae
youngw77 <youngw2354@gmail.com>
2022-10-18 09:13:04 +0900

    ejs board href 수정

--- app/src/routes/board/board.ctrl.js
+++ app/src/routes/board/board.ctrl.js
@@ -17,16 +17,16 @@ const output = {
     },
     table :  (req, res) => {
         // var page = req.params.page; 
-        var mysql = require('../../config/db');
-        var con = mysql.createConnection();
-        var sql = 'SELECT * FROM write'; 
-        con.query(sql, function (err, rows, fields) { 
-            if (err) console.error(err); 
-            else res.render('board/list.ejs', {list : rows}); 
-        });
+        // var mysql = require('../../config/db');
+        // var con = mysql.createConnection();
+        // var sql = "SELECT * FROM socket.write"; 
+        // con.query(sql, function (err, rows, fields) { 
+        //     if (err) console.error(err); 
+        //     else res.render('board/list.ejs', {list : rows}); 
+        // });
     },
     redirect : (req, res) => {
-        var sql = 'SELECT * FROM write'; 
+        var sql = "SELECT * FROM socket.write"; 
         db.query(sql, function (err, rows, fields) { 
             if (err) console.error(err); 
             else res.render('board/list.ejs', {list : rows}); 
