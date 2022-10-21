25400a51aa9e1b8d567e60f475a6ba31755b8422
chobkyu <qudqud97@naver.com>
2022-10-21 11:22:08 +0900

    퍼블리싱

--- app/src/public/js/interceptor/interceptor.js
+++ app/src/public/js/interceptor/interceptor.js
@@ -4,7 +4,7 @@ const interceptor = (req, res, next) => {
     if(!req.session.userId){
         res.send(`
             <script>
-                alert("로그인 페이지로 이동합니다board");
+                alert("로그인 페이지로 이동합니다");
                 location.href="/login";
             </script>            
         `);
